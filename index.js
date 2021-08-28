427c9636d888b882b54ce92a19bd609e3e57bc1c
Your Name <you@example.com>
2021-08-29 04:54:16 +0600

    weather

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-const submitBtn = document.getElementById('submit-btn').addEventListener('click', () => {
+const submitBtn = document.getElementById('submit-btn').addEventListener('click', async () => {
 
     const inputField = document.getElementById('input-field');
     const inputFieldValue = inputField.value;
     inputField.value = "";
 
-    fetch(`https://api.weatherapi.com/v1/current.json?key=7489c7043a2747328d255557212808&q=${inputFieldValue}`)
-        .then(res => res.json())
-        .then(data => cityWeather(data))
+
+    const url = `https://api.weatherapi.com/v1/current.json?key=7489c7043a2747328d255557212808&q=${inputFieldValue}`;
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        cityWeather(data);
+    }
+    catch (error) {
+        console.log(error)
+
+        const containerWeather = document.getElementById('containerWeather');
+        const div = document.createElement('div');
+        div.innerHTML = `
+    <div style="height:50vh" class=" d-flex justify-content-center align-items-center">
+      <div class=" bg-danger rounded-3">
+          <div class="card-body ">
+            <p class="card-text text-white">Please enter a country or city name</p>
+         </div>
+    </div>
+    
+    </div>
+    
+    `
+        containerWeather.appendChild(div);
+
+    }
 })
 
+
+
 const cityWeather = data => {
-    const cityName = data.location.name;
-    const country = data.location.country;
-    const tem = data.current.temp_c;
-    const description = data.current.condition.text;
-    const time = data.location.localtime;
 
-    const iconContainer = document.getElementById('icon');
-    iconContainer.textContent = "";
-    const iconImg = document.createElement('div');
-    iconImg.innerHTML = `
+    const containerWeather = document.getElementById('containerWeather');
+    containerWeather.textContent = "";
+    const div = document.createElement('div');
+    div.innerHTML = `
+
     <img src="${data.current.condition.icon}"></img>
+    <h1 class="text-danger fs-1"><span>${data.location.name}</span> <span>${data.location.country}</span></h1>
+    <p class="text-white">${data.current.temp_c}<span>&deg;c</span> </p>
+    <h4 class="text-warning " >${data.current.condition.text}</h4>
+    <h5 class="text-white">${data.location.localtime}</h5>
+    <h6 class="text-danger">Rukon</h6>
     `
-
-    iconContainer.appendChild(iconImg);
-    document.getElementById('city').innerText = cityName + "-" + country;
-    document.getElementById('temp').innerText = tem;
-    document.getElementById('description').innerText = description;
-    document.getElementById('time').innerText = time;
-
+    containerWeather.appendChild(div);
 
 }
-
-
-// function anotherFunction(){
-
-//        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputFieldValue}&appid=60b4665f25d070ad17f5a9404358462e`)
-
-//         .then(res => res.json())
-//         .then(data => cityWeather(data))
-
-
-// }
